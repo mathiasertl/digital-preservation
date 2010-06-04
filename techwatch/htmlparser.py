0a880cddaa0bfc1cfd654db687df3fdae7d77412
Mathias Ertl <mati@fsinf.at>
2010-06-04 19:33:59 +0000

    default params in constructor are now empty dict, endtag handler doesn't take attrs
    git-svn-id: https://svn.fsinf.at/mati/proj/digital_preservation@609 5e855dc9-1e6c-4aa0-987e-21f7185f60fc

--- techwatch/htmlparser.py
+++ techwatch/htmlparser.py
@@ -1,7 +1,8 @@
 from html.parser import HTMLParser
 
 class Parser( HTMLParser ):
-	def __init__( self, start_handler, startend_handler, end_handler ):
+	def __init__( self, start_handler={}, startend_handler={}, end_handler={} ):
+		HTMLParser.__init__( self )
 		self.start_handler = start_handler # <a ...>
 		self.startend_handler = startend_handler # <img .../>
 		self.end_handler = end_handler # </a>
@@ -14,6 +15,6 @@ class Parser( HTMLParser ):
 		if tag in self.startend_handler.keys():
 			self.startend_handler[tag]( attrs )
 
-	def handle_endtag( self, tag, attrs ):
+	def handle_endtag( self, tag ):
 		if tag in self.end_handler.keys():
-			self.end_handler[tag]( attrs )
+			self.end_handler[tag]()
