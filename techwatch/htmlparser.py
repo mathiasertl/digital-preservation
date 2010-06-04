7ab545b0e26a4e619d30a95fd558835ff3098b6e
Mathias Ertl <mati@fsinf.at>
2010-06-04 18:09:44 +0000

    preliminary .py classes
    git-svn-id: https://svn.fsinf.at/mati/proj/digital_preservation@608 5e855dc9-1e6c-4aa0-987e-21f7185f60fc

---
+++ techwatch/htmlparser.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+
+class Parser( HTMLParser ):
+	def __init__( self, start_handler, startend_handler, end_handler ):
+		self.start_handler = start_handler # <a ...>
+		self.startend_handler = startend_handler # <img .../>
+		self.end_handler = end_handler # </a>
+
+	def handle_starttag( self, tag, attrs ):
+		if tag in self.start_handler.keys():
+			self.start_handler[tag]( attrs )
+
+	def handle_startendtag( self, tag, attrs ):
+		if tag in self.startend_handler.keys():
+			self.startend_handler[tag]( attrs )
+
+	def handle_endtag( self, tag, attrs ):
+		if tag in self.end_handler.keys():
+			self.end_handler[tag]( attrs )
