5584f7064b352554bdea934aa460b7901ef79128
Mathias Ertl <mati@fsinf.at>
2010-06-10 22:03:07 +0000

    send mail notifications
    git-svn-id: https://svn.fsinf.at/mati/proj/digital_preservation@627 5e855dc9-1e6c-4aa0-987e-21f7185f60fc

--- watcher.py
+++ watcher.py
@@ -10,11 +10,28 @@ if os.path.exists( db_path ):
 db = sqlite.backend( db_path )
 recursion_depth = options.get( 'max_depth' )
 
-path = "http://vowi.fsinf.at"
-#path = "http://www.gnu.org/copyleft/fdl.html"
-#path = "http://tubasis.at/~mati/test.html"
-#path = "http://vowi.fsinf.at/wiki?title=FAQ Abkürzungen&oldid=49098"
+path = options.get( 'seed' )
 crawler = htmlcrawler.Crawler( path, db, recursion_depth )
 crawler.crawl()
 
+# waiting for crawl to complete:
 db.queue.join( )
+
+# find rare formats:
+rare_formats = []
+for format in db.formats():
+	percentage = db.percentage( format )
+	if percentage < options.get( 'threshold' ):
+		rare_formats.append( (format, percentage) )
+
+# send mail if one or more fileformats are rare:
+if len(rare_formats) > 0:
+	msg = """The following formats seem to be rare and are below the threshold of %s
+percent:
+
+"""%(options.get( 'threshold' ) )
+	for format in rare_formats:
+		msg += ( "* %s: %.3f percent\n"%(str(format[0], 'utf-8'), format[1]) )
+
+	msg += "\nPlease make a note of it."
+	mail.send( msg )
