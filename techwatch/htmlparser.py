7eea99433d39ed647a469079ae8a79549342b8aa
Mathias Ertl <mati@fsinf.at>
2010-06-10 19:45:35 +0000

    epydoc for techwatch.htmlparser
    git-svn-id: https://svn.fsinf.at/mati/proj/digital_preservation@616 5e855dc9-1e6c-4aa0-987e-21f7185f60fc

--- techwatch/htmlparser.py
+++ techwatch/htmlparser.py
@@ -1,11 +1,23 @@
 from html.parser import HTMLParser
 
 class Parser( HTMLParser ):
+	"""
+	Rather simple implementation of 
+	U{html.parser.HTMLParser<http://docs.python.org/dev/py3k/library/html.parser.html#html.parser.HTMLParser>}.
+	See the constructor for more information. See html.parser.HTMLParser for
+	documentation on the other methods of this class.
+	"""
+
 	def __init__( self, start_handler={}, startend_handler={}, end_handler={} ):
+		"""
+		The constructor allows you to register handlers for each
+		individual start(end)tag. The parameters must be a dictionary
+		with the tags as keys and a method as their value.
+		"""
 		HTMLParser.__init__( self )
-		self.start_handler = start_handler # <a ...>
-		self.startend_handler = startend_handler # <img .../>
-		self.end_handler = end_handler # </a>
+		self.start_handler = start_handler # e.g. <a ...>
+		self.startend_handler = startend_handler # e.g. <img .../>
+		self.end_handler = end_handler # e.g. </a>
 
 	def handle_starttag( self, tag, attrs ):
 		if tag in self.start_handler.keys():
